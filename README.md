69b4a6adaaf7d6ea45d3e5489e73d136120d2b97
Kranthi Mitta <kranthi.mitta@sunrun.global>
2023-03-10 19:54:40 -0800

    code push

--- README.md
+++ README.md
@@ -1,1 +1,155 @@
-# Realestate_Price_Predictor
+<img
+  src="./Images/home.png"
+  alt="Real Estate price predictor"
+  title="Real Estate price predictor"
+  style="display: inline-block; margin: 0 auto; max-width: 60px">
+
+# Real-estate-price-predictor
+
+Real estate price predictor using Machine Learning models - U.S. Mainland, Puerto Rico & U.S. Virgin Islands
+
+
+
+
+## Preview
+
+
+**Project flowchart**
+
+<img
+  src="./Images/app_flowchart.png"
+  alt="App flowchart"
+  title="App flowchart"
+  style="display: inline-block; margin: 0 auto; max-width: 55px">
+  
+
+
+**UI flow - screenshot 01**
+
+<img
+  src="./Images/app_screenshot_01.png"
+  alt="App screenshot 01"
+  title="App screenshot 01"
+  style="display: inline-block; margin: 0 auto; max-width: 55px">
+  
+
+**UI flow - screenshot 02**
+
+<img
+  src="./Images/app_screenshot_02.png"
+  alt="App screenshot 02"
+  title="App screenshot 02"
+  style="display: inline-block; margin: 0 auto; max-width: 55px">
+  
+
+**UI flow - screenshot 03**
+
+<img
+  src="./Images/app_screenshot_03.png"
+  alt="App screenshot 03"
+  title="App screenshot 03"
+  style="display: inline-block; margin: 0 auto; max-width: 55px">
+
+
+
+## Table of contents
+
+- [Objective](#objective)
+- [Preview](#preview)
+- [Environment setup](#environment-setup)
+- [Technologies](#technologies)
+- [Deployment](#deployment)
+- [Contributors](#contributors)
+- [Next steps](#next-steps)
+- [License](#license)
+
+
+
+## Objective
+[(Back to top)](#table-of-contents)
+
+The objective of this project is to predict the selling prices of houses for a real estate app. Static historical data was gathere from Kaggle, on past house sales, including the number of bedrooms, bathrooms, lot acre, house size and state or region in United States, along with the final selling price. 
+The purpose is to implement and deploy machine learning model(s) that can accurately predict the selling price of a house based on its features. Additionally, by using multiple machine learning models, the app evaluates the ML performance against scoring metrics, to provide the user the best ML option. 
+This project aims to help the company make informed decisions based on accurate predictions and improve their business outcomes.
+
+
+
+
+## Environment setup
+[(Back to top)](#table-of-contents)
+
+- `conda create -n [name] python=3.9`
+- `conda activate [name]`
+- `git init` inside a directory where you want to save this repo
+- `git clone` repo
+- `pip install -r requirements.txt`
+
+
+## Technologies
+[(Back to top)](#table-of-contents)
+
+`python`, `anaconda`, `numpy`, `matplotlib`, `pandas`, `streamlit`, `sklearn`
+
+
+
+
+## Deployment
+[(Back to top)](#table-of-contents)
+
+- In Terminal `cd` into cloned repo
+- `cd` to `src` directory where `home.py` file is located
+- `streamlit run home.py`
+- The app will run on `http://localhost:8501/`
+
+
+## Contributors
+[(Back to top)](#table-of-contents)
+
+[Christine Pham](https://github.com/cpham35?tab=repositories) - `cpham35`
+
+[Kranthi C Mitta](https://github.com/kranthicmitta?tab=repositories) - `kranthicmitta` 
+
+[Ayush Jha](https://github.com/jha-ayush?tab=repositories) - `jha-ayush`
+
+
+
+## Next steps
+[(Back to top)](#table-of-contents)
+
+- Initial data capture - better, more robust data sets, time series data, paid APIs.
+- Include attributes like `sq_ft`, `price_per_sq_ft`, `address`, `date_sold`, etc.
+- Use enhanced conformal prediction to find the best ranked sore for price predictions.
+- `Blockchain` implementation: By recording real estate data on a blockchain, we can create a transparent and immutable record of real estate transactions to prevent fraud and corruption, as all transactions are publicly visible and immutable or can't be altered.
+- Rental predictions based on `bed`, `bath` and `sq_ft`.
+- Real-estate portfolio creation and management for investors.
+
+
+
+## License
+[(Back to top)](#table-of-contents)
+
+**MIT License**
+
+Copyright ©  [2023]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+
+<sub>**Note:** This app is for educational purposes only.</sub>
---
+++ docs/ML_functions.py
@@ -0,0 +1,276 @@
+############### Import libraries ###############
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor, ExtraTreesRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LassoCV, Ridge, ElasticNet
+
+
+
+############### Random Forest Regressor ###############
+def random_forest(X, y):
+    """
+    Fits a Random Forest Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """
+    global rf_mean
+    global rf_r2
+    global rf_mae
+    global rf_mse
+    global rf_rmse
+    
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    # Initialize the regressor and fit it to the training data
+    regressor = RandomForestRegressor()
+    regressor.fit(X_train, y_train)
+    
+    # Use the regressor to predict the test data
+    y_pred = regressor.predict(X_test)
+    
+    # Calculate metrics and store them in global variables
+    rf_mean = mean_squared_error(y_test, y_pred)
+    rf_r2 = r2_score(y_test, y_pred)
+    rf_mae = mean_squared_error(y, y_pred)
+    rf_mse = mean_squared_error(y, y_pred)
+    rf_rmse = np.sqrt(mean_squared_error(y, y_pred))
+    
+    return y_pred
+
+
+############### Bagging Regressor ############### 
+def bagging_regressor(X, y):
+    """
+    Fits a Bagging Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """
+    global br_mean
+    global br_r2
+    global br_mae
+    global br_mse
+    global br_rmse
+    
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    # Initialize the base estimator and bagging regressor, and fit it to the training data
+    base_estimator = DecisionTreeRegressor(max_depth=10)
+    bagging_regressor = BaggingRegressor(base_estimator=base_estimator, n_estimators=100, random_state=10)
+    bagging_regressor.fit(X_train, y_train)
+    
+    # Use the regressor to predict the test data
+    y_pred = bagging_regressor.predict(X_test)
+    
+    # Calculate metrics and store them in global variables
+    br_mean = mean_squared_error(y_test, y_pred)
+    br_r2 = r2_score(y_test, y_pred)
+    br_mae = mean_absolute_error(y, y_pred)
+    br_mse = mean_squared_error(y, y_pred)
+    br_rmse = np.sqrt(mean_squared_error(y, y_pred))
+    
+    return y_pred
+
+
+############### Extra Trees Regressor ###############
+def extra_trees_regressor(X, y):    
+    """
+    Fits an Extra Trees Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """
+    global etr_mean
+    global etr_r2
+    global etr_mae
+    global etr_mse
+    global etr_rmse
+    
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+    
+    # Initialize the regressor and fit it to the training data
+    etr_regressor = ExtraTreesRegressor(n_estimators=100, max_depth=10, random_state=10)
+    etr_regressor.fit(X_train, y_train)
+                                       
+    # Use the regressor to predict the test data   
+    y_pred = etr_regressor.predict(X_test)
+                                       
+    # Calculate metrics and store them in global variables
+    etr_mean=mean_squared_error(y_test, y_pred)
+    etr_r2=r2_score(y_test, y_pred)
+    etr_mae = mean_absolute_error(y, y_pred)
+    etr_mse = mean_squared_error(y, y_pred)
+    etr_rmse = np.sqrt(mean_squared_error(y, y_pred))
+                                       
+    return y_pred  
+                                       
+
+############### Gradient Boosting Regressor ###############
+def gradient_boosting_regressor(X, y):
+    """
+    Fits a Gradient Boosting Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """   
+    global gbr_mean
+    global gbr_r2
+    global gbr_mae
+    global gbr_mse
+    global gbr_rmse
+        
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+                                       
+    # Initialize the regressor and fit it to the training data
+    gbr_regressor = GradientBoostingRegressor(n_estimators=50, learning_rate=0.1, max_depth=10, random_state=10)
+    gbr_regressor.fit(X_train, y_train)
+                                       
+    # Use the regressor to predict the test data
+    y_pred = gb_regressor.predict(X_test)
+    
+    # Calculate metrics and store them in global variables
+    gbr_mean=mean_squared_error(y_test, y_pred)
+    gbr_r2=r2_score(y_test, y_pred)
+    gbr_mae = mean_absolute_error(y, y_pred)
+    gbr_mse = mean_squared_error(y, y_pred)
+    gbr_rmse = np.sqrt(mean_squared_error(y, y_pred))
+                                       
+    return y_pred
+
+
+############### LassoCV Regressor ###############
+def lasso_cv(X, y):
+    """
+    Fits a LassoCV Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """   
+    global lasso_mean
+    global lasso_r2
+    global lasso_mae
+    global lasso_mse
+    global lasso_rmse
+        
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+                                       
+    # Initialize the regressor and fit it to the training data
+    lasso_regressor = LassoCV(alphas=10, cv=5, random_state=10)
+    lasso_regressor.fit(X_train, y_train)
+                                       
+    # Use the regressor to predict the test data
+    y_pred = lasso_regressor.predict(X_test)
+       
+    # Calculate metrics and store them in global variables                                   
+    lasso_mean = mean_squared_error(y_test, y_pred)
+    lasso_r2 = r2_score(y_test, y_pred)
+    lasso_mae = mean_absolute_error(y, y_pred)
+    lasso_mse = mean_squared_error(y, y_pred)
+    lasso_rmse = np.sqrt(mean_squared_error(y, y_pred))
+                                       
+    return y_pred                   
+
+
+############### Ridge Regressor ###############
+def ridge_regressor(X, y):
+    """
+    Fits a Ridge Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """ 
+    global ridge_mean
+    global ridge_r2
+    global ridge_mae
+    global ridge_mse
+    global ridge_rmse
+    
+    # Split data into training and test sets                                   
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+                                       
+    # Initialize the regressor and fit it to the training data
+    ridge_regressor = Ridge(alpha=1.0, random_state=10)
+    ridge_regressor.fit(X_train, y_train)
+    
+    # Use the regressor to predict the test data                                   
+    y_pred = ridge_regressor.predict(X_test)
+                                       
+    # Calculate metrics and store them in global variables
+    ridge_mean = mean_squared_error(y_test, y_pred)
+    ridge_r2 = r2_score(y_test, y_pred)
+    ridge_mae = mean_absolute_error(y, y_pred)
+    ridge_mse = mean_squared_error(y, y_pred)
+    ridge_rmse = np.sqrt(mean_squared_error(y, y_pred))
+                                       
+    return y_pred
+
+
+############### ElasticNet Regressor ###############
+def elastic_net_regressor(X, y):
+    """
+    Fits a Elastic Net Regressor model to the input data and returns the predicted values.
+
+    Parameters:
+    X (array-like): The input features to use for training the model.
+    y (array-like): The target variable to predict.
+
+    Returns:
+    y_pred (array-like): The predicted target variable.
+    """ 
+    global enet_mean
+    global enet_r2
+    global enet_mae
+    global enet_mse
+    global enet_rmse
+    
+    # Split data into training and test sets                                   
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
+                                       
+    # Initialize the regressor and fit it to the training data
+    enet_regressor = ElasticNet(alpha=0.1, l1_ratio=0.5, random_state=10)
+    enet_regressor.fit(X_train, y_train)
+    
+    # Use the regressor to predict the test data                                   
+    y_pred = enet_regressor.predict(X_test)
+                                       
+    # Calculate metrics and store them in global variables
+    enet_mean = mean_squared_error(y_test, y_pred)
+    enet_r2 = r2_score(y_test, y_pred)
+    enet_mae = mean_absolute_error(y, y_pred)
+    enet_mse = mean_squared_error(y, y_pred)
+    enet_rmse = np.sqrt(mean_squared_error(y, y_pred))
+                                       
+    return y_pred
